c3fd1a75fe4419b1b86c4f8db16f53644be0d77c
tamas_toth <tothtomi007@gmail.com>
2023-09-12 20:48:18 +0200

    setup Button Control UI

--- app/src/main/java/com/example/szakdolgozat/ButtonControlActivity.kt
+++ app/src/main/java/com/example/szakdolgozat/ButtonControlActivity.kt
@@ -2,6 +2,7 @@ package com.example.szakdolgozat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.example.szakdolgozat.databinding.ActivityButtonControlBinding
 
 class ButtonControlActivity : AppCompatActivity() {
@@ -11,6 +12,7 @@ class ButtonControlActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityButtonControlBinding.inflate(layoutInflater)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/szakdolgozat/MainActivity.kt
+++ app/src/main/java/com/example/szakdolgozat/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.szakdolgozat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.example.szakdolgozat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +13,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(binding.root)
+
+        val intent = Intent(this, ButtonControlActivity::class.java)
+        startActivity(intent)
     }
 }
