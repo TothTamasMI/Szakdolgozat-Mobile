e17ff4da0b2bcb4aa31ed825a1e6d129ee6937f4
tamas_toth <tothtomi007@gmail.com>
2023-10-02 18:32:58 +0200

    connectivity try 1

--- app/src/main/java/com/example/szakdolgozat/ButtonControlActivity.kt
+++ app/src/main/java/com/example/szakdolgozat/ButtonControlActivity.kt
@@ -1,18 +1,113 @@
 package com.example.szakdolgozat
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.ProgressDialog
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
 import android.view.WindowManager
+import androidx.core.app.ActivityCompat
 import com.example.szakdolgozat.databinding.ActivityButtonControlBinding
+import java.io.IOException
+import java.util.UUID
 
 class ButtonControlActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityButtonControlBinding
+
+    companion object {
+        var myUUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+        var bluetoothSocket: BluetoothSocket? = null
+        lateinit var progress: ProgressDialog
+        lateinit var bluetoothAdapter: BluetoothAdapter
+        var isConnected: Boolean = false
+        lateinit var address: String
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityButtonControlBinding.inflate(layoutInflater)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(binding.root)
+
+        address = intent.getStringExtra(ConnectivityActivity.EXTRA_ADDRESS).toString()
+
+        ConnectToDevice(this).execute()
+
+        binding.upButton.setOnTouchListener(object : View.OnTouchListener {
+            @SuppressLint("ClickableViewAccessibility")
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                when (event?.action) {
+                    MotionEvent.ACTION_DOWN ->
+                    sendGoForward()
+                }
+
+                return v?.onTouchEvent(event) ?: true
+            }
+        })
+    }
+    private class ConnectToDevice(c: Context): AsyncTask<Void, Void, String>() {
+        private var isConnectionSuccessed: Boolean = true
+        private val context: Context
+         init {
+             this.context = c
+         }
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            progress = ProgressDialog.show(context, "Connecting...", "please wait")
+        }
+
+        override fun doInBackground(vararg p0: Void?): String? {
+            try {
+                if (bluetoothSocket == null || isConnected){
+                    bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+                    val device: BluetoothDevice = bluetoothAdapter.getRemoteDevice(address)
+                    if (ActivityCompat.checkSelfPermission(
+                            context,
+                            Manifest.permission.BLUETOOTH_CONNECT
+                        ) != PackageManager.PERMISSION_GRANTED
+                    )
+                        bluetoothSocket =device.createInsecureRfcommSocketToServiceRecord(myUUID)
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery()
+                    bluetoothSocket!!.connect()
+                }
+            }catch (e: IOException){
+                isConnectionSuccessed = false
+                e.printStackTrace()
+            }
+            return null
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            if (isConnectionSuccessed) {
+                isConnected = true
+            }
+            progress.dismiss()
+        }
+    }
+
+    fun sendGoForward(){
+        sendCommand("F")
+    }
+
+    private fun sendCommand(input: String){
+        if(bluetoothSocket != null){
+            try{
+                bluetoothSocket!!.outputStream.write(input.toByteArray())
+            }catch(e: IOException){
+                e.printStackTrace()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/szakdolgozat/MainActivity.kt
+++ app/src/main/java/com/example/szakdolgozat/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.szakdolgozat
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowManager
 import com.example.szakdolgozat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,14 +12,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(binding.root)
 
-        /*val intent = Intent(this, ButtonControlActivity::class.java)
-        startActivity(intent)*/
+
 
         binding.connectButton.setOnClickListener {
             val intent = Intent(this, ConnectivityActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.controlButton.setOnClickListener {
+            val intent = Intent(this, ButtonControlActivity::class.java)
             startActivity(intent)
         }
     }
