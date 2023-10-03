3aa0c7546aa03abcff775bc7235b56d718cba238
tamas_toth <tothtomi007@gmail.com>
2023-10-03 10:51:06 +0200

    connectivity done 1.0

--- app/src/main/java/com/example/szakdolgozat/ButtonControlActivity.kt
+++ app/src/main/java/com/example/szakdolgozat/ButtonControlActivity.kt
@@ -2,112 +2,158 @@ package com.example.szakdolgozat
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.app.ProgressDialog
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothSocket
-import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
-import android.os.AsyncTask
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.MotionEvent
-import android.view.View
 import android.view.WindowManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.example.szakdolgozat.databinding.ActivityButtonControlBinding
 import java.io.IOException
+import java.io.OutputStream
 import java.util.UUID
+
 
 class ButtonControlActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityButtonControlBinding
 
     companion object {
-        var myUUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
-        var bluetoothSocket: BluetoothSocket? = null
-        lateinit var progress: ProgressDialog
-        lateinit var bluetoothAdapter: BluetoothAdapter
-        var isConnected: Boolean = false
-        lateinit var address: String
+        private const val DEVICE_ADDRESS = "00:22:08:01:13:CD"
+        private val PORT_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb")
+        private var device: BluetoothDevice? = null
+        private var socket: BluetoothSocket? = null
+        private var outputStream: OutputStream? = null
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityButtonControlBinding.inflate(layoutInflater)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(binding.root)
 
-        address = intent.getStringExtra(ConnectivityActivity.EXTRA_ADDRESS).toString()
-
-        ConnectToDevice(this).execute()
-
-        binding.upButton.setOnTouchListener(object : View.OnTouchListener {
-            @SuppressLint("ClickableViewAccessibility")
-            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-                when (event?.action) {
-                    MotionEvent.ACTION_DOWN ->
-                    sendGoForward()
-                }
-
-                return v?.onTouchEvent(event) ?: true
+        binding.upButton.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                sendForwardCommand()
+            } else if (event.action == MotionEvent.ACTION_UP) {
+                sendDefaultCommand()
             }
-        })
-    }
-    private class ConnectToDevice(c: Context): AsyncTask<Void, Void, String>() {
-        private var isConnectionSuccessed: Boolean = true
-        private val context: Context
-         init {
-             this.context = c
-         }
-
-        override fun onPreExecute() {
-            super.onPreExecute()
-            progress = ProgressDialog.show(context, "Connecting...", "please wait")
+            false
         }
 
-        override fun doInBackground(vararg p0: Void?): String? {
+        binding.leftButton.setOnClickListener {
+            if (setUpBluetooth()) {
+                connectToDevice()
+            }
+        }
+    }
+
+    private fun setUpBluetooth(): Boolean {
+        var found = false
+        val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if (bluetoothAdapter == null) //Checks if the device supports bluetooth
+        {
+            Toast.makeText(
+                applicationContext,
+                "Device doesn't support bluetooth",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        if (!bluetoothAdapter!!.isEnabled) //Checks if bluetooth is enabled. If not, the program will ask permission from the user to enable it
+        {
+            val enableAdapter = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.BLUETOOTH_CONNECT
+                ) != PackageManager.PERMISSION_GRANTED
+            )
+            startActivityForResult(enableAdapter, 0)
             try {
-                if (bluetoothSocket == null || isConnected){
-                    bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-                    val device: BluetoothDevice = bluetoothAdapter.getRemoteDevice(address)
-                    if (ActivityCompat.checkSelfPermission(
-                            context,
-                            Manifest.permission.BLUETOOTH_CONNECT
-                        ) != PackageManager.PERMISSION_GRANTED
-                    )
-                        bluetoothSocket =device.createInsecureRfcommSocketToServiceRecord(myUUID)
-                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery()
-                    bluetoothSocket!!.connect()
-                }
-            }catch (e: IOException){
-                isConnectionSuccessed = false
+                Thread.sleep(1000)
+            } catch (e: InterruptedException) {
                 e.printStackTrace()
             }
-            return null
         }
-
-        override fun onPostExecute(result: String?) {
-            super.onPostExecute(result)
-            if (isConnectionSuccessed) {
-                isConnected = true
+        val bondedDevices = bluetoothAdapter.bondedDevices
+        if (bondedDevices.isEmpty()) //Checks for paired bluetooth devices
+        {
+            Toast.makeText(applicationContext, "Please pair the device first", Toast.LENGTH_SHORT)
+                .show()
+        } else {
+            for (iterator in bondedDevices) {
+                if (iterator.address == DEVICE_ADDRESS) {
+                    device = iterator
+                    found = true
+                    break
+                }
             }
-            progress.dismiss()
+        }
+        return found
+    }
+
+    private fun connectToDevice(): Boolean {
+        var connected = true
+        try {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.BLUETOOTH_CONNECT
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+
+            }
+            socket =
+                device!!.createRfcommSocketToServiceRecord(PORT_UUID) //Creates a socket to handle the outgoing connection
+            socket?.connect()
+            Toast.makeText(
+                applicationContext,
+                "Connection to bluetooth device successful", Toast.LENGTH_LONG
+            ).show()
+        } catch (e: IOException) {
+            e.printStackTrace()
+            connected = false
+        }
+        if (connected) {
+            try {
+                outputStream = socket!!.outputStream //gets the output stream of the socket
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+        return connected
+    }
+
+    private fun sendCommand(command: String){
+        try {
+            outputStream?.write(command.toByteArray())
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
     }
 
-    fun sendGoForward(){
+    private fun sendDefaultCommand(){
+        sendCommand("D")
+    }
+
+    private fun sendForwardCommand(){
         sendCommand("F")
     }
 
-    private fun sendCommand(input: String){
-        if(bluetoothSocket != null){
-            try{
-                bluetoothSocket!!.outputStream.write(input.toByteArray())
-            }catch(e: IOException){
-                e.printStackTrace()
-            }
-        }
+    private fun sendBackwardCommand(){
+        sendCommand("B")
     }
+
+    private fun sendRightCommand(){
+        sendCommand("R")
+    }
+
+    private fun sendLeftCommand(){
+        sendCommand("L")
+    }
+
 }
